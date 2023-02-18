2436656f48c679efda0daa219737e85e629f7915
m.keshavarz <keshavarz.ma13@gmail.com>
2023-02-18 12:30:49 +0330

    Implement open

--- lib/file.c
+++ lib/file.c
@@ -70,7 +70,23 @@ open(const char *path, int mode)
 	// file descriptor.
 
 	// LAB 5: Your code here
-	panic ("open not implemented");
+	struct Fd *fd;
+    int r;
+	if(strlen(path) >= MAXPATHLEN) {
+		return -E_BAD_PATH;
+	}
+	r = fd_alloc(&fd);
+	if (r < 0) {
+		return r;
+	}
+	fsipcbuf.open.req_omode = mode;
+	strcpy(fsipcbuf.open.req_path, path);
+	r = fsipc(FSREQ_OPEN, fd);
+	if (r < 0) {
+		fd_close(fd, 0);
+		return r;	
+	}
+	return fd2num(fd);
 }
 
 // Flush the file descriptor.  After this the fileid is invalid.
@@ -102,11 +118,12 @@ devfile_read(struct Fd *fd, void *buf, size_t n)
 	// system server.
 	// LAB 5: Your code here
     int r;
-    fsipcbuf.read.req_fileid = fd->fd_file.id;
-    fsipcbuf.read.req_n = n;
-	if ((r = fsipc(FSREQ_READ, NULL)) < 0) 
-        return r;
-    memmove(buf, fsipcbuf.readRet.ret_buf, r);
+	fsipcbuf.write.req_fileid = fd->fd_file.id;
+	fsipcbuf.write.req_n = n;
+	memmove((void*)fsipcbuf.write.req_buf, (void*)buf, n);
+	if ((r = fsipc(FSREQ_WRITE, NULL)) < 0){
+		return r;
+	}
 	return r;
 }
 
